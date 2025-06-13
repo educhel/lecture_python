71b160121e80c9f412b5c62e104cec4664363509
educhel <node.chelsea@gmail.com>
2025-06-13 15:09:13 +0900

    solution practice

--- day05/practice_lotto.py
+++ day05/practice_lotto.py
@@ -1,1 +1,11 @@
 # 카운트 다운이 있는 로또 번호 추첨기 만들기
+import random
+import time
+
+for i in range(5):
+    print(f'{5-i}초 남았습니다.')
+    time.sleep(1)
+
+lotto = list(random.sample(range(1,46),6))
+lotto.sort() 
+print(lotto)
