dedcf3eb941e621ed1d1c418ded1ae7ccd7facfc
educhel <node.chelsea@gmail.com>
2025-06-16 17:31:09 +0900

    practice oop

--- day06/practice_oop.py
+++ day06/practice_oop.py
@@ -1,15 +1,27 @@
 # 입금과 출금, 조회
 
+# 케이스 1
 class BankAccount:
     def __init__(self, account_holder, balance=0):
         self.account_holder = account_holder 
         self.balance = balance
 
     def deposit(self, amount):
-        self.balance += amount
+        if type(amount) == int and amount > 0:
+            self.balance += amount
+        else:
+            print('잘못된 값입니다.')
     
     def withdraw(self, amount):
-        self.balance -= amount
+        if type(amount) == int and amount > 0:
+            if amount <= self.balance:
+                self.balance -= amount
+            else:
+                print('잔액이 부족합니다.')
+        else:
+            print('잘못된 값입니다.')
+        
+# 케이스 2
 
 # 객체 생성 및 메서드 호출
 account = BankAccount("홍길동")
